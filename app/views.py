377bf595e21b76ef544bf29af4d182bdfb22486d
Norah.Waswala <norah.waswala@student.moringa.com>
2022-05-02 18:56:57 +0300

    feat: call the get movie function

--- app/views.py
+++ app/views.py
@@ -1,5 +1,6 @@
 from flask import render_template
 from app import app
+from .request import get_news
 
 # Views
 @app.route('/')
@@ -8,4 +9,7 @@ def index():
     '''
     View root page function that returns the index page and its data
     '''
-    return render_template('index.html')
+
+    popular_movies = get_movies('popular')
+    print(popular_movies)
+    return render_template('index.html',top_headlines)
