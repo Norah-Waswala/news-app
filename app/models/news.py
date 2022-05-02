864e740bfd71f689972426ff9f2ab883944bcd28
Norah.Waswala <norah.waswala@student.moringa.com>
2022-05-02 18:46:56 +0300

    feat: add models class to contain news file

---
+++ app/models/news.py
@@ -0,0 +1,9 @@
+class News:
+    def __init__(self,author,title,description,urlToImage,date,content,url):
+        self.author=author
+        self.title=title
+        self.description=description
+        self.urlToImage=urlToImage
+        self.date=date
+        self.content=content
+        self.url=url
