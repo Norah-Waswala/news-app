fe8a0ab98348172c8c38ad682e06d18bfc98ebb1
Norah.Waswala <norah.waswala@student.moringa.com>
2022-05-04 15:37:28 +0300

    refactor:change articles functionality

--- app/views.py
+++ app/views.py
@@ -1,22 +1,25 @@
 from flask import render_template
 from app import app
-from .request import get_news,sources,get_articles
-
-
+from .request import get_articles, get_news, get_sources
 @app.route('/')
 def index():
     news = get_news()
-    source=sources("sources")
+    source=get_sources("sources")
     '''
     View root page function that returns the index page and its data
     '''
-    return render_template('index.html',articles=news,sources=source )
+    return render_template('index.html',articles=news, sources=source)
+# @app.route("/articles/<name>")
+# def body(name):
+    # bbc=get_articles("abc-news")
+    # abc=get_articles("abc-news-au")
+    # aljezera=get_articles("al-jazeera-english")
+    # technica=get_articles("ars-technica")
+    # return render_template("articles.html",name=name,bbc_news=bbc,abc_news=abc,aljezera_n=aljezera,tech=technica)
 
-@app.route("/articles/<name>")
-def body(name):
-    bbc=get_articles("abc-news")
-    abc=get_articles("abc-news-au")
-    aljezera=get_articles("al-jazeera-english")
-    technica=get_articles("ars-technica")
 
-    return render_template("articles.html",name=name,bbc_news=bbc,abc_news=abc,aljezera_n=aljezera,tech=technica)
+
+
+
+
+
